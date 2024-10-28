818ed26f8ea8ad43e695d3a7e1c0f7ad62569bb7
Arvelmay <arvelmo6@gmail.com>
2024-10-28 10:00:56 -0500

    IT WORKKKKEEEE

--- Unit 2/Project/PIApprox.java
+++ Unit 2/Project/PIApprox.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PIApprox
 {
     public static double gottPI(int digits)
@@ -5,15 +7,22 @@ public class PIApprox
         double temp = 1;
         double alsoTemp = 1;
         double addTo = 0;
-        for (int i = digits; i>0; i--)
+        for (int i = 1; i<=digits; i++)
         {
             temp = 4*(1.0/alsoTemp);
-            if (i%2 == 2)
+            if (i%2 == 0)
                 addTo-= temp;
             else{
                 addTo+=temp;}
             alsoTemp+=2;
         }
         return addTo;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scannyboi = new Scanner(System.in);
+        System.out.println(gottPI(scannyboi.nextInt()));
+        scannyboi.close();
     }
 }
