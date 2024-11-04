90045230412c2f5b96da97dac1be284499831815
Arvelmay <123535960+Arvelmay@users.noreply.github.com>
2024-11-04 10:17:53 -0600

    D:

--- Unit 3/Coding/RectClient.java
+++ Unit 3/Coding/RectClient.java
@@ -3,5 +3,7 @@ public class RectClient
     public static void main(String[] args) {
         Rectangle recty = new Rectangle(3,7);
         Rectangle waitAMinuteThatsNotActuallyARectangleIThinkThatIsActuallyASquare = new Rectangle(4,4);
+        Rectangle whatWhyIsThereNoParamsINeedParamsWHEREARETHEPARAMS = new Rectangle();
+        System.out.println(whatWhyIsThereNoParamsINeedParamsWHEREARETHEPARAMS.diagonal());
     }
 }
