f9ba3a6b0b3e55c806169a61b7dd2395f0d004d8
Arvelmay <123535960+Arvelmay@users.noreply.github.com>
2024-11-11 09:44:25 -0600

    CAT FIX

--- Unit 3/Coding/Cat.java
+++ Unit 3/Coding/Cat.java
@@ -1,22 +1,43 @@
-//imports
 
-public class Cat {
-    //Instance varibales
+public class Cat
+{
+    //Instance Variables
+    //CAT FACTS!
     private String color;
     private int age;
     private String name;
     private double weight;
-
+    
+    
     //Constructors
-    //Sets default or initial values
-    public cat(){
+    //The purpose of a constructor is to set the inital value for the instance variables.
+    
+    //Default constructor (or no-arg or no-param)
+    //Create default cat with no info provided.
+    public Cat(){
         this.color = "unknown color";
         this.age = 5;
         this.name = "unknown name";
         this.weight = 10;
     }
-    public cat(){
-        
+    //User-defined constructor
+    public Cat(String col, int ag, String nm, double wt){
+        this.color = col;
+        this.age = ag;
+        this.name = nm;
+        this.weight = wt;
     }
-
+    
+    public void meow(){
+        System.out.println(this.name+": 'MEOW'");}
+    
 }
+
+
+
+
+
+
+
+
+
