fbf4dea7b22cc8dd50e53efe49135bbb96956552
Arvelmay <123535960+Arvelmay@users.noreply.github.com>
2024-12-09 09:48:23 -0600

    a

---
+++ Unit 4/coding/FirstArray.java
@@ -0,0 +1,6 @@
+public class FirstArray {
+    public static void main(String[] args) {
+        String[] arraa = new String[]{"helop", "goof", "AAAAAAAAA"};
+        System.out.println(arraa);
+    }
+}
