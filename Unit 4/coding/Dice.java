0732f226803ebc97273dae51379aebd3c50c6872
Arvelmay <123535960+Arvelmay@users.noreply.github.com>
2024-12-16 09:16:50 -0600

    guhhh

---
+++ Unit 4/coding/Dice.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+/**
+ * Write a description of class Dice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Dice
+{
+    public static int roll2 (){
+        Random randyboi = new Random();
+        return (randyboi.nextInt()+randyboi.nextInt());
+    }
+    
+    public static int[] rollAgain (int times){
+        int[] ints = new int[times];
+        for (int i = 0; i < times; i++){
+            ints[i] = roll2();
+        }
+        return ints;
+    }
+    
+    
+}
