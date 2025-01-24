8d82d23fe39705c294961361131b1a12d50413ab
Arvelmay <Arvelmo@gmail.com>
2025-01-24 09:33:17 -0600

    finshed the squar

--- Unit 4/project/magicSquares.java
+++ Unit 4/project/magicSquares.java
@@ -12,21 +12,30 @@ public class magicSquares {
         int magicNum = 0;
         int newNum = 0;
 
+
+        for (int c = 0; c < squar.length; c++){
+            magicNum += squar[0][c];
+        }
+
         for(int r = 0; r < squar.length; r++){
             for (int c = 0; c < squar[r].length; c++){
-                magicNum+=squar[r][c];
+                newNum+=squar[r][c];
             }
+            if (newNum != magicNum){
+                return false;
+            }
+            newNum = 0;
         }
 
         for (int c = 0; c < squar.length; c++){
             for (int r = 0; r < squar.length; r++){
                 newNum += squar[r][c];
             }
+            if (newNum != magicNum){
+                return false;
+            }
+            newNum = 0;
         }
-        if (newNum != magicNum){
-            return false;
-        }
-        newNum = 0;
 
         for (int r = 0; r < squar.length; r++)
         {
@@ -52,13 +61,14 @@ public class magicSquares {
 
 
     public static void main(String[] args) {
-        int [][] ints1 = {{2},{4}};
-        int[][] ints2 = {{3}};
+        int[][] ints1 = {{3}};
+        int[][] ints2 = {{2,7,6},{9,5,1},{4,3,8}};
         int[][] ints3 = {{2,4},{3,1}};
-        int[][] ints4 = {{2,7,6},{}}
+        int[][] ints4 = {{5,8,3},{7,4,9}};
 
         System.out.println(isMagic(ints1));
         System.out.println(isMagic(ints2));
         System.out.println(isMagic(ints3));
+        System.out.println(isMagic(ints4));
     }
 }
