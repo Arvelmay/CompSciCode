2d207ea308d4d0a4c1042a35f98bab4f068b628a
Arvelmay <arvelmo6@gmail.com>
2025-02-06 10:38:26 -0600

    pokey pokey poker no more

--- Unit 5/project/Game.java
+++
@@ -1,7 +0,0 @@
-public class Game {
-    public static void main(String[] args){
-        Deck deckyboi = new Deck();
-
-        System.out.println(deckyboi);
-    }
-}
---
+++ Unit 5/project/Monster.java
@@ -0,0 +1,8 @@
+
+
+public class Monster {
+    private int hp;
+    private int ap;
+    private int mp;
+    private int pp;
+}
